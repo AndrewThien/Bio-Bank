a7f9f49c5cb47ad2e2a8de9d0f30c11a888da889
Tri Thien Nguyen <anretrithien@gmail.com>
2023-12-13 20:28:55 +0000

    render collections table on the homepage

---
+++ src/app/api/collections/route.ts
@@ -0,0 +1,21 @@
+import { db } from "@/lib/db";
+import { collections } from "@/lib/db/schema";
+import { eq } from "drizzle-orm";
+import { NextResponse } from "next/server";
+
+export const runtime = "edge";
+
+export const GET = async (req: Request) => {
+
+  try {
+
+    const _collections = await db
+    .select()
+    .from(collections);
+
+    return NextResponse.json(_collections);
+  } catch (error: any) {
+    console.log(error, "Fetching collections is going wrong")
+    return NextResponse.json({error: "Fetching collections is going wrong"}, {status: 500}); 
+  }
+}
