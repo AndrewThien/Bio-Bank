b9499ecfbd3b618ff899eeb1dd0eada74f5a09bc
Tri Thien Nguyen <anretrithien@gmail.com>
2023-12-31 19:24:13 +0000

    modify loading page's  layout

--- src/components/LoadingPage.tsx
+++ src/components/LoadingPage.tsx
@@ -2,8 +2,8 @@ import { Loader2 } from 'lucide-react';
 
 function LoadingPage() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-b from-sky-400 to-sky-200">
-      <div className="flex items-center text-2xl">
+    <div className="flex flex-col items-center min-h-screen bg-gradient-to-b from-sky-400 to-sky-200">
+      <div className="mt-5 flex items-center text-2xl">
         <h1>Loading</h1>  
         <Loader2 className="h-10 w-10 animate-spin ml-2" /> 
       </div>
